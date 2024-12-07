3951e5d061424c0b534fb7d86a185763359aa158
Daveian <[dguxdesigns@gmail.com]>
2024-12-06 22:33:25 -0500

    Add ProjectLibrary and form handling for dynamic project creation
    - Imported ProjectLibrary to manage projects
    - Created new Project instances and added them to the library
    - Set up form submission to add new projects dynamically
    - Integrated rendering of updated project list after adding new projects
    - Updated project list display with newly added projects
    - Added basic validation for project name input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import './style.css';
-import { renderProjects } from './render';
+import { RenderProjects } from './render';
+import { Project } from './projects';
+import { ProjectLibrary } from './projectLibrary';
 
-const projectList = new renderProjects('[data-projects]');
-let tags = ['Work', 'Family'];
+const projectList = new RenderProjects('[data-projects]');
+const projects = [
+    new Project('Work', null),
+    new Project('Family', null)
+]
+const myProjects = new ProjectLibrary(projects);
+const getProjects = myProjects.getProjects();
 
-projectList.renderList(tags);
+const newListForm = document.querySelector('[data-new-project-form]');
+const newListInput = document.querySelector('[data-new-project-input]')
 
 
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const project = {name: newListInput.value.trim(), tasks: null};
+
+    if (project.name === null || project.name === '') {
+        return;
+    }
+
+    newListInput.value = null;
+    myProjects.addNewProject(project);
+    projectList.renderList(getProjects);
+
+    console.table(projects);
+})
+
+projectList.renderList(getProjects);
