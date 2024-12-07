cae8fda49b0bff660cc959b81bddd3c5226d9242
Daveian <[dguxdesigns@gmail.com]>
2024-12-06 21:14:55 -0500

    Initialize renderProjects module and render project list
    - Imported style.css and renderProjects module
    - Created instance of renderProjects to render project list
    - Initialized project tags array and passed it to renderList
    - Set up dynamic rendering of initial project tags

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import { renderProjects } from './render';
+
+const projectList = new renderProjects('[data-projects]');
+let tags = ['Work', 'Family'];
+
+projectList.renderList(tags);
+
+
